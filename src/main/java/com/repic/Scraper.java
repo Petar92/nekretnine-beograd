ccf3ca541571b78f89b1eecb473f9f55ac95f6fe
Petar92 <repic92@gmail.com>
2020-11-30 21:23:44 +0100

    a

--- src/main/java/com/repic/Scraper.java
+++ src/main/java/com/repic/Scraper.java
@@ -1,7 +1,10 @@
 package com.repic;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,20 +14,69 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Scraper {
 	
-	public void scrapeNekretnineRS() {
+	private List<String> results = Collections.synchronizedList(new ArrayList<>());
+	
+	public void scrapeNekretnineRS(int numberOfThreads) {
 		
-		Integer pageNumber = 1;
-		String baseUrl = "https://www.nekretnine.rs/" ;
-		List<String> results = new ArrayList<>();
+		Integer totalItems;
+		Integer totalPages;
 		
 		WebClient client = new WebClient();  
 		client.getOptions().setCssEnabled(false);  
 		client.getOptions().setJavaScriptEnabled(false); 
-		while (true) {
-			try {  
-			  String searchUrl = "https://www.nekretnine.rs/stambeni-objekti/stanovi/izdavanje-prodaja/prodaja/grad/beograd/lista/po-stranici/10/stranica/"
-					  + pageNumber + "/";
+				
+		try {
+			  String searchUrl = "https://www.nekretnine.rs/stambeni-objekti/stanovi/izdavanje-prodaja/prodaja/grad/beograd/lista/po-stranici/10/stranica/";
 			  HtmlPage page = client.getPage(searchUrl);
+			  
+			  HtmlElement totalElements = ((HtmlElement) page.getFirstByXPath(".//a[@class='radio filter-radio active']/span"));
+			  
+			  String te = totalElements.asText();
+			  te = te.replace("(", "");
+			  te = te.replace(")", "");
+			  totalItems = Integer.parseInt(te);
+			  System.out.println("total items " + totalItems);
+			  totalPages = 500; //(int) Math.ceil(totalItems / 20.0);
+			  
+			  List<Thread> threads = new ArrayList<>();
+				for(int i = 0; i < numberOfThreads ; i++) {
+					int div = (int) (Math.ceil((double) totalPages / numberOfThreads));
+					int startPage = i * div + 1;
+					int endPage = i * div + div;
+					Thread thread = new Thread(() -> {
+						long start = System.nanoTime();
+						scrapePage(startPage, endPage, searchUrl, client, results);
+						long end = System.nanoTime();
+						System.out.println("thread time " + (end - start) / 1000000000);
+					});
+					threads.add(thread);
+				}
+				
+		        for(Thread thread : threads) {
+		            thread.start();
+		        }
+
+				
+				for(Thread thread : threads) { 
+					try { 
+						thread.join(); 
+					} catch(InterruptedException e) { } 
+				}
+			  
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+		
+	private static void scrapePage(int startPage, int endPage, String searchUrl, WebClient client, List<String> results) {
+		try {
+		  int currentPage = startPage;	
+		  searchUrl = "https://www.nekretnine.rs/stambeni-objekti/stanovi/izdavanje-prodaja/prodaja/grad/beograd/lista/po-stranici/10/stranica/" + currentPage + "/";
+		  //System.out.println("SEARCH URL ---------------------->" + searchUrl);
+		  HtmlPage page = client.getPage(searchUrl);
+		  
+		  while (currentPage <= endPage) {
+			  System.out.println("current page: " + currentPage + ", end page: " + endPage);
 			  List<HtmlElement> items = (List<HtmlElement>) (Object) page.getByXPath("//div[@class='row offer']") ;
 			  if(items.isEmpty()){
 					System.out.println("breaking...");
@@ -41,7 +93,7 @@ public class Scraper {
 						String itemPrice = spanPrice == null ? "0.0" : spanPrice.asText() ;
 						
 						Item item = new Item();
-						
+						synchronized(results) {
 						itemArea = itemArea.replace(" ", "");
 						itemArea = itemArea.replace("---", "0");
 						itemArea = itemArea.replace("m2", "");
@@ -49,26 +101,41 @@ public class Scraper {
 						
 						itemPrice = itemPrice.replace("---", "0");
 						itemPrice = itemPrice.replace("EUR", "");
-						itemPrice = itemPrice.replace(" ", "");				
+						itemPrice = itemPrice.replace(" ", "");
+						itemPrice = itemPrice.replace("Po dogovoru", "");	
 						item.setPrice(new BigDecimal(itemPrice));
 					
 						
 						ObjectMapper mapper = new ObjectMapper();
 						String jsonString = mapper.writeValueAsString(item);
-						results.add(jsonString);
+						
+							results.add(jsonString);
+						}
 						//System.out.println(jsonString);
 					}
-					pageNumber++;
-				}
-			}catch(Exception e){
-			  e.printStackTrace();
-			}
+					currentPage++;
+			  }
+		  }
+		}catch(Exception e){
+		  e.printStackTrace();
 		}
-		System.out.println(results);
+		//System.out.println(results);
+synchronized(results) {
 		System.out.println("result size: " + results.size());
+}
 	}
 	public static void main(String[] args) {
-		Scraper s = new Scraper();
-		s.scrapeNekretnineRS();
+		PrintStream originalOut = System.out;
+		PrintStream fileOut;
+		try {
+			fileOut = new PrintStream("/home/petar/out.txt");
+			System.setOut(fileOut);
+			Scraper s = new Scraper();
+			s.scrapeNekretnineRS(10);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
